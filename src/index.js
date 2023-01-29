1c2737805fe5b883db1eccd364b3cf1ad3c0d019
pi0neerpat <blockchainbuddha@gmail.com>
2023-01-28 18:33:52 -0800

    add redirect url

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
 // see previous example for the things that are not commented
-require('dotenv').config();
+require("dotenv").config();
 
-const assert = require('assert');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const Provider = require('oidc-provider');
+const assert = require("assert");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const Provider = require("oidc-provider");
 
-assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
-assert(process.env.PORT, 'process.env.PORT missing');
-assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
-assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.HEROKU_APP_NAME, "process.env.HEROKU_APP_NAME missing");
+assert(process.env.PORT, "process.env.PORT missing");
+assert(
+  process.env.SECURE_KEY,
+  "process.env.SECURE_KEY missing, run `heroku addons:create securekey`"
+);
+assert.equal(
+  process.env.SECURE_KEY.split(",").length,
+  2,
+  "process.env.SECURE_KEY format invalid"
+);
 
-const jwks = require('./jwks.json');
+const jwks = require("./jwks.json");
 // simple account model for this application, user list is defined like so
-const Account = require('./account');
+const Account = require("./account");
 
-const oidc = new Provider('https://node-oidc-provider-example.vercel.app', {
+const oidc = new Provider("https://node-oidc-provider-example.vercel.app", {
   clients: [
     {
-      client_id: '123',
+      client_id: "123",
       // client_secret: "node-oidc-secret",
-      redirect_uris: ['https://jwt.io', 'http://localhost/redirect/node_oidc', 'http://0.0.0.0:3000/redirect/node_oidc', 'http://localhost:3000/redirect/node_oidc', 'http://0.0.0.0:8910/redirect/node_oidc',
-        'http://localhost:8910/redirect/node_oidc', 'https://oauth2-client-redwood-eta.vercel.app/redirect/node_oidc'], // using jwt.io as redirect_uri to show the ID Token contents
-      response_types: ['code'],
-      grant_types: ['authorization_code'],
-      token_endpoint_auth_method: 'none',
+      redirect_uris: [
+        "https://jwt.io",
+        "https://oauth2-server-redwood-web.onrender.com/redirect/node_oidc",
+        "http://localhost/redirect/node_oidc",
+        "http://0.0.0.0:3000/redirect/node_oidc",
+        "http://localhost:3000/redirect/node_oidc",
+        "http://0.0.0.0:8910/redirect/node_oidc",
+        "http://localhost:8910/redirect/node_oidc",
+        "https://oauth2-client-redwood-eta.vercel.app/redirect/node_oidc",
+      ], // using jwt.io as redirect_uri to show the ID Token contents
+      response_types: ["code"],
+      grant_types: ["authorization_code"],
+      token_endpoint_auth_method: "none",
     },
   ],
   cookies: {
-    keys: process.env.SECURE_KEY.split(','),
+    keys: process.env.SECURE_KEY.split(","),
   },
   jwks,
   // oidc-provider only looks up the accounts by their ID when it has to read the claims,
@@ -40,8 +55,8 @@ const oidc = new Provider('https://node-oidc-provider-example.vercel.app', {
   // let's tell oidc-provider you also support the email scope, which will contain email and
   // email_verified claims
   claims: {
-    openid: ['sub'],
-    email: ['email', 'email_verified'],
+    openid: ["sub"],
+    email: ["email", "email_verified"],
   },
 
   // let's tell oidc-provider where our own interactions will be
@@ -74,152 +89,182 @@ oidc.proxy = true;
 
 // let's work with express here, below is just the interaction definition
 const expressApp = express();
-expressApp.set('trust proxy', true);
-expressApp.set('view engine', 'ejs');
-expressApp.set('views', path.resolve(__dirname, 'views'));
+expressApp.set("trust proxy", true);
+expressApp.set("view engine", "ejs");
+expressApp.set("views", path.resolve(__dirname, "views"));
 
 const parse = bodyParser.urlencoded({ extended: false });
 
 function setNoCache(req, res, next) {
-  res.set('Pragma', 'no-cache');
-  res.set('Cache-Control', 'no-cache, no-store');
+  res.set("Pragma", "no-cache");
+  res.set("Cache-Control", "no-cache, no-store");
   next();
 }
 
-expressApp.get('/interaction/:uid', setNoCache, async (req, res, next) => {
+expressApp.get("/interaction/:uid", setNoCache, async (req, res, next) => {
   try {
     const details = await oidc.interactionDetails(req, res);
     // eslint-disable-next-line no-console
-    console.log('see what else is available to you for interaction views', details);
-    const {
-      uid, prompt, params,
-    } = details;
+    console.log(
+      "see what else is available to you for interaction views",
+      details
+    );
+    const { uid, prompt, params } = details;
 
     const client = await oidc.Client.find(params.client_id);
 
-    if (prompt.name === 'login') {
-      return res.render('login', {
+    if (prompt.name === "login") {
+      return res.render("login", {
         client,
         uid,
         details: prompt.details,
         params,
-        title: 'Sign-in',
+        title: "Sign-in",
         flash: undefined,
       });
     }
 
-    return res.render('interaction', {
+    return res.render("interaction", {
       client,
       uid,
       details: prompt.details,
       params,
-      title: 'Authorize',
+      title: "Authorize",
     });
   } catch (err) {
     return next(err);
   }
 });
 
-expressApp.post('/interaction/:uid/login', setNoCache, parse, async (req, res, next) => {
-  try {
-    const details = await oidc.interactionDetails(req, res);
-    console.log(
-      'see what else is available to you for interaction views',
-      details,
-    );
-    const { uid, prompt, params } = details;
-    assert.strictEqual(prompt.name, 'login');
-    const client = await oidc.Client.find(params.client_id);
+expressApp.post(
+  "/interaction/:uid/login",
+  setNoCache,
+  parse,
+  async (req, res, next) => {
+    try {
+      const details = await oidc.interactionDetails(req, res);
+      console.log(
+        "see what else is available to you for interaction views",
+        details
+      );
+      const { uid, prompt, params } = details;
+      assert.strictEqual(prompt.name, "login");
+      const client = await oidc.Client.find(params.client_id);
 
-    const accountId = await Account.authenticate(req.body.email, req.body.password);
+      const accountId = await Account.authenticate(
+        req.body.email,
+        req.body.password
+      );
 
-    if (!accountId) {
-      res.render('login', {
-        client,
-        uid,
-        details: prompt.details,
-        params: {
-          ...params,
-          login_hint: req.body.email,
-        },
-        title: 'Sign-in',
-        flash: 'Invalid email or password.',
-      });
-      return;
-    }
-
-    const result = {
-      login: { accountId },
-    };
-
-    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
-  } catch (err) {
-    next(err);
-  }
-});
-
-expressApp.post('/interaction/:uid/confirm', setNoCache, parse, async (req, res, next) => {
-  try {
-    const interactionDetails = await oidc.interactionDetails(req, res);
-    const { prompt: { name, details }, params, session: { accountId } } = interactionDetails;
-    assert.strictEqual(name, 'consent');
-
-    let { grantId } = interactionDetails;
-    let grant;
-
-    if (grantId) {
-      // we'll be modifying existing grant in existing session
-      grant = await oidc.Grant.find(grantId);
-    } else {
-      // we're establishing a new grant
-      grant = new oidc.Grant({
-        accountId,
-        clientId: params.client_id,
-      });
-    }
-
-    if (details.missingOIDCScope) {
-      grant.addOIDCScope(details.missingOIDCScope.join(' '));
-      // use grant.rejectOIDCScope to reject a subset or the whole thing
-    }
-    if (details.missingOIDCClaims) {
-      grant.addOIDCClaims(details.missingOIDCClaims);
-      // use grant.rejectOIDCClaims to reject a subset or the whole thing
-    }
-    if (details.missingResourceScopes) {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const [indicator, scopes] of Object.entries(details.missingResourceScopes)) {
-        grant.addResourceScope(indicator, scopes.join(' '));
-        // use grant.rejectResourceScope to reject a subset or the whole thing
+      if (!accountId) {
+        res.render("login", {
+          client,
+          uid,
+          details: prompt.details,
+          params: {
+            ...params,
+            login_hint: req.body.email,
+          },
+          title: "Sign-in",
+          flash: "Invalid email or password.",
+        });
+        return;
       }
+
+      const result = {
+        login: { accountId },
+      };
+
+      await oidc.interactionFinished(req, res, result, {
+        mergeWithLastSubmission: false,
+      });
+    } catch (err) {
+      next(err);
     }
+  }
+);
 
-    grantId = await grant.save();
+expressApp.post(
+  "/interaction/:uid/confirm",
+  setNoCache,
+  parse,
+  async (req, res, next) => {
+    try {
+      const interactionDetails = await oidc.interactionDetails(req, res);
+      const {
+        prompt: { name, details },
+        params,
+        session: { accountId },
+      } = interactionDetails;
+      assert.strictEqual(name, "consent");
 
-    const consent = {};
-    if (!interactionDetails.grantId) {
-      // we don't have to pass grantId to consent, we're just modifying existing one
-      consent.grantId = grantId;
+      let { grantId } = interactionDetails;
+      let grant;
+
+      if (grantId) {
+        // we'll be modifying existing grant in existing session
+        grant = await oidc.Grant.find(grantId);
+      } else {
+        // we're establishing a new grant
+        grant = new oidc.Grant({
+          accountId,
+          clientId: params.client_id,
+        });
+      }
+
+      if (details.missingOIDCScope) {
+        grant.addOIDCScope(details.missingOIDCScope.join(" "));
+        // use grant.rejectOIDCScope to reject a subset or the whole thing
+      }
+      if (details.missingOIDCClaims) {
+        grant.addOIDCClaims(details.missingOIDCClaims);
+        // use grant.rejectOIDCClaims to reject a subset or the whole thing
+      }
+      if (details.missingResourceScopes) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [indicator, scopes] of Object.entries(
+          details.missingResourceScopes
+        )) {
+          grant.addResourceScope(indicator, scopes.join(" "));
+          // use grant.rejectResourceScope to reject a subset or the whole thing
+        }
+      }
+
+      grantId = await grant.save();
+
+      const consent = {};
+      if (!interactionDetails.grantId) {
+        // we don't have to pass grantId to consent, we're just modifying existing one
+        consent.grantId = grantId;
+      }
+
+      const result = { consent };
+      await oidc.interactionFinished(req, res, result, {
+        mergeWithLastSubmission: true,
+      });
+    } catch (err) {
+      next(err);
     }
-
-    const result = { consent };
-    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: true });
-  } catch (err) {
-    next(err);
   }
-});
+);
 
-expressApp.get('/interaction/:uid/abort', setNoCache, async (req, res, next) => {
-  try {
-    const result = {
-      error: 'access_denied',
-      error_description: 'End-User aborted interaction',
-    };
-    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
-  } catch (err) {
-    next(err);
+expressApp.get(
+  "/interaction/:uid/abort",
+  setNoCache,
+  async (req, res, next) => {
+    try {
+      const result = {
+        error: "access_denied",
+        error_description: "End-User aborted interaction",
+      };
+      await oidc.interactionFinished(req, res, result, {
+        mergeWithLastSubmission: false,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // leave the rest of the requests to be handled by oidc-provider, there's a catch all 404 there
 expressApp.use(oidc.callback());
